dced389ec2ed2b2ffe9ad6ed475eb14f880cba87
2eg4rizve <2eg4rizve.com>
2023-10-30 23:50:00 +0600

    logout

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(
   cors({
-    origin: ['http://localhost:5173' ,'http://localhost:5174' ],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
@@ -37,28 +37,25 @@ const logger = async (req, res, next) => {
 };
 
 const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
-  console.log('value of token in middleware : ' , token);
+  const token = req?.cookies?.token;
+  console.log("value of token in middleware : ", token);
   if (!token) {
     return res.status(401).send({ message: "not authorized" });
   }
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     //error
-    if(err){
-      return res.status(401).send({message: 'unauthorized'})
+    if (err) {
+      return res.status(401).send({ message: "unauthorized" });
     }
-     
+
     //if token is valid then it would be decoded
 
-    console.log('Value in the decoded token ',decoded );
+    console.log("Value in the decoded token ", decoded);
 
     req.user = decoded;
 
     next();
-
-  })
-
-
+  });
 };
 
 async function run() {
@@ -72,7 +69,7 @@ async function run() {
     //auth related api jwt
     app.post("/jwt", logger, async (req, res) => {
       const user = req.body;
-      console.log(user);
+      console.log("User for token: ", user);
 
       //making token
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -91,6 +88,14 @@ async function run() {
         .send({ success: true });
     });
 
+    // logout and cookies clear
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logout and clear cookies  user email : ", user)
+      res.clearCookie('token',{maxAge: 0}) .send({success: true});
+
+    });
+
     //services related api
     app.get("/services", logger, async (req, res) => {
       const cursor = serviceCollection.find();
@@ -99,7 +104,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/services/:id",  async (req, res) => {
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
 
@@ -118,8 +123,8 @@ async function run() {
       // console.log("email when booking from valid token: ", req.user);
       // console.log("token when booking : ", req.cookies.token);
 
-      if(req.query.email != req.user.email){
-        return res.status(403).send({message: 'forbidden access'})
+      if (req.query.email != req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
       let query = {};
       if (req.query?.email) {
@@ -183,3 +188,11 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Car Doctor Server Is Running On Port ${port}`);
 });
+
+// npm i jsonwebtoken
+
+// node
+//require('crypto).randomBytes(64).toString('hex')
+
+
+//npm i cookie-parser
